820abe8a64544ba9dc1877beba8eebad95cff732
AlmostSomeone <almostsome1@gmail.com>
2022-03-02 14:58:55 +0100

    Messages now support placeholders

--- src/main/java/dev/almostsomeone/premiumpvp/commands/KitPvPCMD.java
+++ src/main/java/dev/almostsomeone/premiumpvp/commands/KitPvPCMD.java
@@ -4,11 +4,13 @@ import dev.almostsomeone.premiumpvp.Main;
 import dev.almostsomeone.premiumpvp.utilities.Config;
 import dev.almostsomeone.premiumpvp.utilities.Messages;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 
 import java.util.HashMap;
 
 import static dev.almostsomeone.premiumpvp.utilities.ChatUtil.color;
+import static dev.almostsomeone.premiumpvp.utilities.ChatUtil.format;
 
 public class KitPvPCMD extends CommandBuilder {
 
@@ -31,50 +33,56 @@ public class KitPvPCMD extends CommandBuilder {
 
     @Override
     public boolean execute(CommandSender sender, String label, String[] args) {
+        if(!(sender instanceof Player)) {
+            sender.sendMessage(color(messages.getMessage("global.only-players")));
+            return true;
+        }
+        Player player = (Player) sender;
+
         if(args.length == 0) {
-            sender.sendMessage(color(this.messages.getMessage("commands.use-help").replaceAll("%command%", "/" + label)));
+            player.sendMessage(format(player, this.messages.getMessage("commands.help.use-help").replaceAll("\\{command\\}", "/" + label)));
             return true;
         } else {
             switch(args[0].toLowerCase()) {
                 case "info":
-                    sender.sendMessage(" ");
-                    sender.sendMessage("Welcome to PremiumPvP v" + this.plugin.getDescription().getVersion());
-                    sender.sendMessage("Made by AlmostSomeone");
-                    sender.sendMessage("https://github.com/AlmostSomeone/PremiumPvP");
-                    sender.sendMessage(" ");
+                    player.sendMessage(" ");
+                    player.sendMessage("Welcome to PremiumPvP v" + this.plugin.getDescription().getVersion());
+                    player.sendMessage("Made by AlmostSomeone");
+                    player.sendMessage("https://github.com/AlmostSomeone/PremiumPvP");
+                    player.sendMessage(" ");
                     break;
                 case "help":
-                    sender.sendMessage(color(this.messages.getMessage("commands.helpmenu.header")
-                            .replaceAll("%command%", "/" + label)));
+                    sender.sendMessage(format(player, this.messages.getMessage("commands.help.header")
+                            .replaceAll("\\{command\\}", "/" + label)));
                     this.subCommands.forEach((subcommand, description) ->
-                            sender.sendMessage(color(this.messages.getMessage("commands.helpmenu.item")
-                                    .replaceAll("%command%", "/" + label)
-                                    .replaceAll("%subcommand%", subcommand.substring(0,1).toUpperCase() + subcommand.substring(1).toLowerCase())
-                                    .replaceAll("%description%", description))));
-                    sender.sendMessage(color(this.messages.getMessage("commands.helpmenu.footer")
-                            .replaceAll("%command%", "/" + label)));
+                            player.sendMessage(format(player, this.messages.getMessage("commands.help.item")
+                                    .replaceAll("\\{command\\}", "/" + label)
+                                    .replaceAll("\\{subcommand\\}", subcommand.substring(0,1).toUpperCase() + subcommand.substring(1).toLowerCase())
+                                    .replaceAll("\\{description\\}", description))));
+                    player.sendMessage(format(player, this.messages.getMessage("commands.help.footer")
+                            .replaceAll("\\{command\\}", "/" + label)));
                     break;
                 case "reload":
                     try {
                         this.config.reload();
                         this.messages.reload();
-                        sender.sendMessage(color(this.messages.getMessage("commands.kitpvp.config.reload-success")));
+                        player.sendMessage(format(player, this.messages.getMessage("commands.kitpvp.config.reload-success")));
                     } catch (Exception exception) {
                         exception.printStackTrace();
-                        sender.sendMessage(color(this.messages.getMessage("commands.kitpvp.config.reload-failed")));
+                        player.sendMessage(format(player, this.messages.getMessage("commands.kitpvp.config.reload-failed")));
                     }
                     break;
                 case "save":
                     try {
                         //TODO Save data
-                        sender.sendMessage(color(this.messages.getMessage("commands.kitpvp.data.save-success")));
+                        player.sendMessage(format(player, this.messages.getMessage("commands.kitpvp.data.save-success")));
                     } catch (Exception exception) {
                         exception.printStackTrace();
-                        sender.sendMessage(color(this.messages.getMessage("commands.kitpvp.data.save-failed")));
+                        player.sendMessage(format(player, this.messages.getMessage("commands.kitpvp.data.save-failed")));
                     }
                     break;
                 default:
-                    sender.sendMessage(color(this.messages.getMessage("commands.use-help").replaceAll("%command%", "/" + label)));
+                    player.sendMessage(format(player, this.messages.getMessage("commands.help.use-help").replaceAll("\\{command\\}", "/" + label)));
                     break;
             }
         }
--- src/main/java/dev/almostsomeone/premiumpvp/commands/WorldCMD.java
+++ src/main/java/dev/almostsomeone/premiumpvp/commands/WorldCMD.java
@@ -3,8 +3,6 @@ package dev.almostsomeone.premiumpvp.commands;
 import dev.almostsomeone.premiumpvp.Main;
 import dev.almostsomeone.premiumpvp.utilities.Messages;
 import org.bukkit.Bukkit;
-import org.bukkit.Location;
-import org.bukkit.Material;
 import org.bukkit.World;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
@@ -13,6 +11,7 @@ import org.bukkit.event.player.PlayerTeleportEvent;
 import java.util.HashMap;
 
 import static dev.almostsomeone.premiumpvp.utilities.ChatUtil.color;
+import static dev.almostsomeone.premiumpvp.utilities.ChatUtil.format;
 
 public class WorldCMD extends CommandBuilder {
 
@@ -28,23 +27,25 @@ public class WorldCMD extends CommandBuilder {
 
     @Override
     public boolean execute(CommandSender sender, String label, String[] args) {
-        if(this.getPermission() != null && !sender.hasPermission(this.getPermission())) {
-            sender.sendMessage(color(this.getPermissionMessage()));
-            return true;
-        }
         if(!(sender instanceof Player)) {
             sender.sendMessage(color(messages.getMessage("global.only-players")));
             return true;
         }
+        Player player = (Player) sender;
+
+        if(this.getPermission() != null && !player.hasPermission(this.getPermission())) {
+            player.sendMessage(color(this.getPermissionMessage()));
+            return true;
+        }
         if(args.length < 1 || args.length > 2) {
-            sender.sendMessage(color(messages.getMessage("commands.world.usage")));
+            player.sendMessage(format(player, messages.getMessage("commands.world.usage")));
             return true;
         } else {
             if(args.length == 1) {
                 try {
-                    teleport(((Player) sender), Bukkit.getWorld(args[0]));
+                    teleport(player, Bukkit.getWorld(args[0]));
                 } catch (NullPointerException exception) {
-                    sender.sendMessage(color(messages.getMessage("commands.world.no-world").replaceAll("%world%", args[0])));
+                    player.sendMessage(format(player, messages.getMessage("commands.world.no-world")));
                     return true;
                 }
             } else {
@@ -52,14 +53,14 @@ public class WorldCMD extends CommandBuilder {
                 try {
                     target = Bukkit.getPlayerExact(args[1]);
                 } catch (Exception exception) {
-                    sender.sendMessage(color(messages.getMessage("global.target-not-online")));
+                    player.sendMessage(format(player, messages.getMessage("global.target-not-online")));
                     return true;
                 }
                 if(target != null) {
                     try {
                         teleport(target, Bukkit.getWorld(args[0]));
                     } catch (NullPointerException exception) {
-                        sender.sendMessage(color(messages.getMessage("commands.world.no-world").replaceAll("%world%", args[0])));
+                        player.sendMessage(format(player, messages.getMessage("commands.world.no-world")));
                         return true;
                     }
                 }
@@ -70,6 +71,6 @@ public class WorldCMD extends CommandBuilder {
 
     private void teleport(Player player, World world) throws NullPointerException {
         player.teleport(world.getHighestBlockAt(world.getSpawnLocation()).getLocation(), PlayerTeleportEvent.TeleportCause.PLUGIN);
-        player.sendMessage(color(messages.getMessage("commands.world.teleported").replaceAll("%world%", world.getName())));
+        player.sendMessage(format(player, messages.getMessage("commands.world.teleported")));
     }
 }
