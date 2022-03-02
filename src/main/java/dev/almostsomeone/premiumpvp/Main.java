fa3b399bdda3cb3d8953b067ce232b3ec0149d91
AlmostSomeone <almostsome1@gmail.com>
2022-03-02 12:23:08 +0100

    Fixed the onStartup and onStarted, added NMS in onStatup

--- src/main/java/dev/almostsomeone/premiumpvp/Main.java
+++ src/main/java/dev/almostsomeone/premiumpvp/Main.java
@@ -4,9 +4,11 @@ import dev.almostsomeone.premiumpvp.commands.KitPvPCMD;
 import dev.almostsomeone.premiumpvp.commands.WorldCMD;
 import dev.almostsomeone.premiumpvp.common.bukkit.world.VoidGenerator;
 import dev.almostsomeone.premiumpvp.common.bukkit.world.WorldManager;
+import dev.almostsomeone.premiumpvp.common.nms.NMS;
 import dev.almostsomeone.premiumpvp.utilities.*;
 import org.bstats.bukkit.Metrics;
 import org.bstats.charts.SimplePie;
+import org.bukkit.Bukkit;
 import org.bukkit.generator.ChunkGenerator;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -27,6 +29,11 @@ public class Main extends JavaPlugin {
 
     @Override
     public void onEnable() {
+        this.onStartup();
+        Bukkit.getScheduler().scheduleSyncDelayedTask(this, this::onStarted);
+    }
+
+    private void onStartup() {
         // Loading the configuration and messages
         this.getLogger().log(Level.INFO, "Loading configuration...");
         this.config = new Config(this);
@@ -38,27 +45,32 @@ public class Main extends JavaPlugin {
         String language = (this.config.get().isSet("language") ? this.config.get().getString("language") : "EN");
         metrics.addCustomChart(new SimplePie("language", () -> language));
 
+        // Hooking into NMS
+        new NMS(this);
+    }
+
+    private void onStarted() {
         // Load Worlds
         this.getLogger().log(Level.INFO, "Loading worlds...");
-        worldManager = new WorldManager(this);
+        this.worldManager = new WorldManager(getInstance());
         this.getLogger().log(Level.INFO, "Loaded all worlds");
 
-        // Starting the updater
-        new Updater(this);
-
         // Initialize all commands
-        new KitPvPCMD(this);
+        new KitPvPCMD(getInstance());
         new WorldCMD();
+
+        // Starting the updater
+        new Updater(getInstance());
 
         // Inform the server administrators about possible issues with non-stable releases
         String version = this.getDescription().getVersion();
-        if(version.contains("-")) {
+        if (version.contains("-")) {
             String suffix = version.split("-")[1].toLowerCase();
             this.getLogger().log(Level.WARNING, "You are running a " + suffix + " release. Consider using our latest stable release for public servers.");
         }
     }
 
-    public static Main getInstance(){
+    public static Main getInstance() {
         return Main.getPlugin(Main.class);
     }
 }
