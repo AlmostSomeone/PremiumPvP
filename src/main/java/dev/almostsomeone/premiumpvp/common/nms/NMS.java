7e8f5b723eaaac9cf2f8feabeb2068c75c85380b
AlmostSomeone <almostsome1@gmail.com>
2022-03-02 13:39:39 +0100

    Cleanup in NMS class

--- src/main/java/dev/almostsomeone/premiumpvp/common/nms/NMS.java
+++ src/main/java/dev/almostsomeone/premiumpvp/common/nms/NMS.java
@@ -7,22 +7,23 @@ import java.util.logging.Level;
 
 public class NMS {
 
-    private final Plugin plugin;
     private GlobalClassBuilder globalClassBuilder;
 
     public NMS(final Plugin plugin) {
-        this.plugin = plugin;
-
         String version = plugin.getServer().getClass().getPackage().getName();
         version = version.substring(version.lastIndexOf('.') + 1);
-        this.plugin.getLogger().log(Level.INFO, "Hooking into NMS " + version);
+        plugin.getLogger().log(Level.INFO, "Hooking into NMS " + version);
 
         this.globalClassBuilder = new GlobalClassBuilder();
         try {
             globalClassBuilder.init(version, plugin);
         } catch (ClassNotFoundException exception) {
-            this.plugin.getLogger().log(Level.WARNING, "Failed to hook into NMS " + version + ". Please check if you are running a supported fork, if you are, contact the developer.");
+            plugin.getLogger().log(Level.WARNING, "Failed to hook into NMS " + version + ". Please check if you are running a supported fork, if you are, contact the developer.");
             exception.printStackTrace();
         }
+    }
+
+    public GlobalClassBuilder getClasses(){
+        return globalClassBuilder;
     }
 }
