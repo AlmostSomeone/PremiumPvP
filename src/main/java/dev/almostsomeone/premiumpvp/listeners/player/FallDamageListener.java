633ff50c16b137163f4196a164c2463cfacfbf14
n2g7mutf8 <95335602+n2g7mutf8@users.noreply.github.com>
2022-06-24 21:05:44 +0700

    Added a new FallDamageListener
    This commit is for the fall-damage options in the settings.yml

---
+++ src/main/java/dev/almostsomeone/premiumpvp/listeners/player/FallDamageListener.java
@@ -0,0 +1,32 @@
+package dev.almostsomeone.premiumpvp.listeners.player;
+
+import dev.almostsomeone.premiumpvp.Main;
+import dev.almostsomeone.premiumpvp.game.Game;
+import dev.almostsomeone.premiumpvp.game.gameplayer.GamePlayerManager;
+import dev.almostsomeone.premiumpvp.world.WorldManager;
+import dev.almostsomeone.premiumpvp.world.WorldProfile;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.FoodLevelChangeEvent;
+
+public class FallDamageListener implements Listener {
+
+    @EventHandler
+    public void foodLevelChange(FoodLevelChangeEvent event) {
+        if(!(event.getEntity() instanceof Player player)) return;
+
+        // Don't do anything if the player is not in the game
+        Game game = Main.getGame();
+        GamePlayerManager gamePlayerManager = game.getGamePlayerManager();
+        if(gamePlayerManager.getGamePlayer(player.getUniqueId()) == null) return;
+
+        // Don't do anything if the world has no profile
+        WorldManager worldManager = game.getWorldManager();
+        WorldProfile worldProfile = worldManager.getWorldProfile(player.getWorld().getName());
+        if(worldProfile == null) return;
+
+        if(!worldProfile.getFallDamage())
+            event.setCancelled(true);
+    }
+}
