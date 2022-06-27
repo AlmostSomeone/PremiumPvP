4e88cd8de6f3b93cd8e36ee18246a9ea19dc4b8a
AlmostSomeone <almostsome1@gmail.com>
2022-06-27 09:41:57 +0200

    FoodLevel and FallDamage listeners now check GamePlayerState

--- src/main/java/dev/almostsomeone/premiumpvp/listeners/player/FallDamageListener.java
+++ src/main/java/dev/almostsomeone/premiumpvp/listeners/player/FallDamageListener.java
@@ -2,7 +2,9 @@ package dev.almostsomeone.premiumpvp.listeners.player;
 
 import dev.almostsomeone.premiumpvp.Main;
 import dev.almostsomeone.premiumpvp.game.Game;
+import dev.almostsomeone.premiumpvp.game.gameplayer.GamePlayer;
 import dev.almostsomeone.premiumpvp.game.gameplayer.GamePlayerManager;
+import dev.almostsomeone.premiumpvp.game.gameplayer.GamePlayerState;
 import dev.almostsomeone.premiumpvp.world.WorldManager;
 import dev.almostsomeone.premiumpvp.world.WorldProfile;
 import org.bukkit.entity.Player;
@@ -20,14 +22,13 @@ public class FallDamageListener implements Listener {
         // Don't do anything if the player is not in the game
         Game game = Main.getGame();
         GamePlayerManager gamePlayerManager = game.getGamePlayerManager();
-        if(gamePlayerManager.getGamePlayer(player.getUniqueId()) == null) return;
+        GamePlayer gamePlayer = gamePlayerManager.getGamePlayer(player.getUniqueId());
+        if(gamePlayer == null || gamePlayer.getGamePlayerState().equals(GamePlayerState.NONE)) return;
 
         // Don't do anything if the world has no profile
         WorldManager worldManager = game.getWorldManager();
         WorldProfile worldProfile = worldManager.getWorldProfile(player.getWorld().getName());
-        if(worldProfile == null) return;
-
-        if(!worldProfile.getFallDamage())
+        if(worldProfile != null && !worldProfile.getFallDamage())
             event.setCancelled(true);
     }
 }
--- src/main/java/dev/almostsomeone/premiumpvp/listeners/player/FoodLevelChangeListener.java
+++ src/main/java/dev/almostsomeone/premiumpvp/listeners/player/FoodLevelChangeListener.java
@@ -1,6 +1,8 @@
 package dev.almostsomeone.premiumpvp.listeners.player;
 
 import dev.almostsomeone.premiumpvp.Main;
+import dev.almostsomeone.premiumpvp.game.gameplayer.GamePlayer;
+import dev.almostsomeone.premiumpvp.game.gameplayer.GamePlayerState;
 import dev.almostsomeone.premiumpvp.world.WorldManager;
 import dev.almostsomeone.premiumpvp.world.WorldProfile;
 import dev.almostsomeone.premiumpvp.game.Game;
@@ -19,14 +21,13 @@ public class FoodLevelChangeListener implements Listener {
         // Don't do anything if the player is not in the game
         Game game = Main.getGame();
         GamePlayerManager gamePlayerManager = game.getGamePlayerManager();
-        if(gamePlayerManager.getGamePlayer(player.getUniqueId()) == null) return;
+        GamePlayer gamePlayer = gamePlayerManager.getGamePlayer(player.getUniqueId());
+        if(gamePlayer == null || gamePlayer.getGamePlayerState().equals(GamePlayerState.NONE)) return;
 
         // Don't do anything if the world has no profile
         WorldManager worldManager = game.getWorldManager();
         WorldProfile worldProfile = worldManager.getWorldProfile(player.getWorld().getName());
-        if(worldProfile == null) return;
-
-        if(!worldProfile.getHunger())
+        if(worldProfile != null && !worldProfile.getHunger())
             event.setCancelled(true);
     }
 }
